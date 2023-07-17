ab71da66d78a098d30cf8a200d829a5bd276c4c3
ah-2010 <138315547+ah-2010@users.noreply.github.com>
2023-07-17 20:00:04 +0400

    carakusi class

---
+++ carakusi/src/Carakusi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Carakusi {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("tpeq tiv ev es kasem dra qarakusin");
+
+
+        double number = sc.nextDouble();
+
+
+        double patasxan = number * number;
+        System.out.println(patasxan);
+
+
+    }
+}
